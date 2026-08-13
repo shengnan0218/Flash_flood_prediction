eefb1ee704f6f8f546507ac92dab5c8349e8c0bf
shengnan0218 <150988149+shengnan0218@users.noreply.github.com>
2026-08-13 12:03:41 +0800

    Export frozen monotone TRAIN rating module

--- models/observation/__init__.py
+++ models/observation/__init__.py
@@ -1,9 +1,10 @@
 from .independent_delta_z import IndependentDeltaZHead
 from .monotonic_qz import MonotonicQZObservation
-from .train_fitted_rating import TrainFittedLinearRating
+from .train_fitted_rating import TrainFittedLinearRating, TrainFittedMonotoneRating
 
 __all__ = [
     "IndependentDeltaZHead",
     "MonotonicQZObservation",
     "TrainFittedLinearRating",
+    "TrainFittedMonotoneRating",
 ]
