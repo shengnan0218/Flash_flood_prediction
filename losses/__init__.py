6702ac7b71f3514d5b1269a7cc4c4ea8b27e5dff
shengnan0218 <150988149+shengnan0218@users.noreply.github.com>
2026-08-13 09:41:29 +0800

    Route training through P3 consistency loss adapter

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,7 +1,4 @@
-from .flood_multitask_loss import (
-    FloodMultitaskLoss,
-    LossTerm,
-    water_level_first_differences,
-)
+from .flood_multitask_loss import LossTerm, water_level_first_differences
+from .p3_consistency import FloodMultitaskLoss
 
 __all__ = ["FloodMultitaskLoss", "LossTerm", "water_level_first_differences"]
