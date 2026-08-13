5418352693e17417a3e6f11b9613f2b5dce2ca5a
shengnan0218 <150988149+shengnan0218@users.noreply.github.com>
2026-08-13 11:19:41 +0800

    Export TRAIN-fitted rating curve

--- models/observation/__init__.py
+++ models/observation/__init__.py
@@ -1,4 +1,9 @@
 from .independent_delta_z import IndependentDeltaZHead
 from .monotonic_qz import MonotonicQZObservation
+from .train_fitted_rating import TrainFittedLinearRating
 
-__all__ = ["IndependentDeltaZHead", "MonotonicQZObservation"]
+__all__ = [
+    "IndependentDeltaZHead",
+    "MonotonicQZObservation",
+    "TrainFittedLinearRating",
+]
