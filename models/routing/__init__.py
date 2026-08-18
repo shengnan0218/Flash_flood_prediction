14d685cb3cdb692bf106ffbd5ff79a82e668236f
shengnan0218 <150988149+shengnan0218@users.noreply.github.com>
2026-08-18 16:50:42 +0800

    Route formal kinematic wave through optimized implementation

--- models/routing/__init__.py
+++ models/routing/__init__.py
@@ -1,1 +1,4 @@
-from .kinematic_wave import KinematicWaveGNN, PureDirectedGNN, EdgeParameterNetwork
+from .kinematic_wave import EdgeParameterNetwork, PureDirectedGNN
+from .kinematic_wave_optimized import KinematicWaveGNN
+
+__all__ = ["KinematicWaveGNN", "PureDirectedGNN", "EdgeParameterNetwork"]
